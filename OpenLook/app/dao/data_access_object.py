4e78cbfc0d1d8454b43793035fb8cb69688d93dc
lara <lara.marzi@gmail.com>
2020-08-12 16:53:51 +0200

    update search

--- OpenLook/app/dao/data_access_object.py
+++ OpenLook/app/dao/data_access_object.py
@@ -18,7 +18,10 @@ class DataAccess:
         results = self.solr.search(user_input, rows=100)
         docs = results.docs
         df = pd.DataFrame(docs)
-        string_param = param_group.replace("_db_map", "") #to get the litteral value
+        if "publisher" in param_group or "creator" in param_group:
+            string_param = param_group.replace("_db_map", "") #to get the litteral value
+        else:
+            string_param = param_group.replace("_db_map", "_ss")
         new_df = df[[param_group, string_param, "id"]] #create new dataframe with just the desired values
         exploded = new_df.set_index(['id']).apply(pd.Series.explode).reset_index() #EXPLODE the listed values!! see https://stackoverflow.com/a/59330040/5102877
         group = exploded.dropna().groupby([param_group, string_param])["id"].count() #see https://realpython.com/pandas-groupby/
